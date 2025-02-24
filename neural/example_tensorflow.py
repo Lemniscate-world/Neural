79a3835b2f6d444c1a0c589588000426df39ee9f
Lemniscate-SHA-256 <Lemniscate_zero@proton.me>
2025-02-24 21:40:58 +0000

    Fixes in the code_generator concerning tensorflow runtime errors for input_shape and symbolic shapes interpretations for None

--- neural/example_tensorflow.py
+++ neural/example_tensorflow.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 model = tf.keras.Sequential(name='MyModel', layers=[
-    tf.keras.layers.Input(shape=(None, 28, 28)),
+    tf.keras.layers.Dense(units=128, activation='relu', input_shape=(None, 28, 28)),
     tf.keras.layers.Dense(units=128, activation='relu'),
     tf.keras.layers.Dropout(rate=0.2),
     tf.keras.layers.Dense(units=10, activation='softmax'),
