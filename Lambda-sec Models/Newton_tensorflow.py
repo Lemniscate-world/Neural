437ca605f537a3ef7d03452b58a75f6b1bb93c03
Lemniscate-SHA-256 <Lemniscate_zero@proton.me>
2025-02-25 18:07:47 +0000

    Fixes for layers expanding in code generator

--- Lambda-sec Models/Newton_tensorflow.py
+++ Lambda-sec Models/Newton_tensorflow.py
@@ -1,6 +1,16 @@
 import tensorflow as tf
 
 model = tf.keras.Sequential(name='MathTransformer', layers=[
+    # Transformer Encoder
+                encoder_input = tf.keras.Input(shape=(512,))
+                attention = tf.keras.layers.MultiHeadAttention(
+                    num_heads=8, key_dim=2048//8)(encoder_input, encoder_input)
+                x = tf.keras.layers.Dropout(0.1)(attention)
+                x = tf.keras.layers.LayerNormalization(epsilon=1e-6)(x + encoder_input)
+                x = tf.keras.layers.Dense(2048, activation='relu')(x)
+                x = tf.keras.layers.Dense(512)(x)
+                x = tf.keras.layers.LayerNormalization(epsilon=1e-6)(x)
+                model.add(tf.keras.Model(inputs=encoder_input, outputs=x))
     tf.keras.layers.Dense(units=512, activation='relu'),
     tf.keras.layers.Dense(units=10, activation='softmax'),
 ])
