313225e4526e92bc43d258ddd6d929d43ab9fc87
Lemniscate-SHA-256 <Lemniscate_zero@proton.me>
2025-03-11 10:33:02 +0000

    Fix typo in GROUPNORMALIZATION key in parser

--- neural/parser/parser.py
+++ neural/parser/parser.py
@@ -502,7 +502,7 @@ class ModelTransformer(lark.Transformer):
             'GAUSSIANNOISE': 'gaussian_noise',
             'LAYERNORMALIZATION': 'layer_norm',
             'INSTANCENORMALIZATION': 'instance_norm',
-            'GROUPNORAMLIZATION': 'group_norm',
+            'GROUPNORMALIZATION': 'group_norm',
         }
         self.hpo_params = []
 
